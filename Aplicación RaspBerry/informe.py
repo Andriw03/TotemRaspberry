0ec0f2f207987d4dce922178a787d39d8b951b2d
Andres <ctorresa0720@gmail.com>
2023-03-29 10:50:28 -0300

    add fecha a informe

--- Aplicación RaspBerry/informe.py
+++ Aplicación RaspBerry/informe.py
@@ -1,19 +1,27 @@
 import json
 import schedule
 import time
+from datetime import datetime
 import requests
 import pandas as pd
+
 from servicios import ApiTotem
 
+fecha = datetime.now()
+fecha = fecha.strftime('%m-%Y')
+print(fecha)
 
 def obtener_datos_y_exportar_excel():
     response = ApiTotem.getApi('informeflujo')
 
     df = pd.read_json(json.dumps(response))
-    df.to_excel('/home/codelco/Desktop/Django/Pendrive/txttotem/clientes.xlsx', index=False)
-obtener_datos_y_exportar_excel()
-# schedule.every().month.at('10:00').do(obtener_datos_y_exportar_excel)
+    df.to_excel(f'/home/codelco/Desktop/Django/Pendrive/txttotem/informe-{fecha}.xlsx', index=False)
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+obtener_datos_y_exportar_excel()
+
+schedule.every().month.at('06:00').do(obtener_datos_y_exportar_excel)
+
+while True:
+     schedule.run_pending()
+     time.sleep(1)
+
